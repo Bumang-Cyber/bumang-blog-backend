89497dc951fb8d13f2170e16fec9c168ce87a50e
Bumang-Cyber <calmness0729@gmail.com>
2025-04-17 00:28:12 +0900

    feat: usersModule, commentsModule에서 AuthModule 참조 삭제

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,10 +3,9 @@ import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentEntity } from './entities/comment.entity';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentEntity]), AuthModule],
+  imports: [TypeOrmModule.forFeature([CommentEntity])],
   controllers: [CommentsController],
   providers: [CommentsService],
   exports: [CommentsService],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,9 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), AuthModule],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
