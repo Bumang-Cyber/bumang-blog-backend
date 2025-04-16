fc62fde2f6c3713b3843479d4b6e70544ccfc19b
Bumang-Cyber <calmness0729@gmail.com>
2025-04-17 00:30:52 +0900

    chore: commentsModule의 forFeature에 PostEntity 추가

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,9 +3,10 @@ import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentEntity } from './entities/comment.entity';
+import { PostEntity } from 'src/posts/entities/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentEntity])],
+  imports: [TypeOrmModule.forFeature([CommentEntity, PostEntity])],
   controllers: [CommentsController],
   providers: [CommentsService],
   exports: [CommentsService],
