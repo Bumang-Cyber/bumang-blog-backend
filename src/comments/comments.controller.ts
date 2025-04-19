1777a27eee8529ac834e3a1a4e59c9173029de70
Bumang-Cyber <calmness0729@gmail.com>
2025-04-19 16:21:33 +0900

    feat: comment, tag 스웨거 description 추가

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -12,7 +12,9 @@ import { CreateCommentDto } from './dto/create-comment.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { IsOwner } from 'src/auth/decorators/is-owner.decorator';
 import { IsOwnerGuard } from 'src/auth/guards/is-owner.guard';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Comments') // Swagger UI에서 그룹 이름
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
@@ -21,12 +23,20 @@ export class CommentsController {
    * @COMMENTS
    */
   @Get('post/:id')
+  @ApiOperation({
+    summary: '코멘트 조회',
+    description: '특정 게시물에 달린 코멘트 조회합니다.',
+  })
   async findAllGroups(@Param('id', ParseIntPipe) id: number) {
     return await this.commentsService.findCommentsInPostDetail(id);
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('post/:id')
+  @ApiOperation({
+    summary: '코멘트 생성',
+    description: '특정 게시물에 달린 코멘트를 생성합니다.',
+  })
   async createOneComment(
     @Param('id', ParseIntPipe) id: number,
     @Body() { content }: CreateCommentDto,
@@ -37,6 +47,10 @@ export class CommentsController {
   @UseGuards(JwtAuthGuard, IsOwnerGuard)
   @IsOwner('comment')
   @Post(':id')
+  @ApiOperation({
+    summary: '코멘트 수정',
+    description: '특정 게시물에 달린 코멘트를 수정합니다.',
+  })
   async updateOneComment(
     @Param('id', ParseIntPipe) id: number,
     @Body() { content }: CreateCommentDto,
@@ -47,6 +61,10 @@ export class CommentsController {
   @UseGuards(JwtAuthGuard, IsOwnerGuard)
   @IsOwner('comment')
   @Post(':id')
+  @ApiOperation({
+    summary: '코멘트 삭제',
+    description: '특정 게시물에 달린 코멘트를 삭제합니다.',
+  })
   async deleteOneComment(@Param('id', ParseIntPipe) id: number) {
     return await this.commentsService.deleteOneComment(id);
   }
--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -17,7 +17,9 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { RolesEnum } from 'src/users/const/roles.const';
 import { Roles } from 'src/auth/decorators/roles.decorator';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('tags') // Swagger UI에서 그룹 이름
 @Controller('tags')
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
@@ -25,16 +27,25 @@ export class TagsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(RolesEnum.ADMIN)
   @Post()
+  @ApiOperation({ summary: '태그 생성', description: '태그를 생성합니다.' })
   async create(@Body() createTagDto: CreateTagDto) {
     return await this.tagsService.createTag(createTagDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: '모든 태그 조회',
+    description: '모든 태그를 조회합니다.',
+  })
   async findAllTag() {
     return await this.tagsService.findAllTag();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: '특정 태그 조회',
+    description: '특정 태그를 조회합니다.',
+  })
   async findOneTag(@Param('id', ParseIntPipe) id: number) {
     return await this.tagsService.findOneTag(id);
   }
@@ -42,6 +53,10 @@ export class TagsController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(RolesEnum.ADMIN)
   @Patch(':id')
+  @ApiOperation({
+    summary: '특정 태그 수정',
+    description: '특정 태그를 수정합니다.',
+  })
   async updateOneTag(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateTagDto: UpdateTagDto,
@@ -53,6 +68,10 @@ export class TagsController {
   @Roles(RolesEnum.ADMIN)
   @Delete(':id')
   @HttpCode(204)
+  @ApiOperation({
+    summary: '특정 태그 삭제',
+    description: '특정 태그를 삭제합니다.',
+  })
   async removeOneTag(@Param('id', ParseIntPipe) id: number) {
     return await this.tagsService.removeOneTag(id);
   }
