1d601c0e7dd5a650eecc4547eeaef8a2eed9087f
Bumang-Cyber <calmness0729@gmail.com>
2025-04-28 23:37:01 +0900

    feat: migration sql문 수정

--- src/migrations/20250428123000-AddReadPermissionToPost.ts
+++ src/migrations/20250428123000-AddReadPermissionToPost.ts
@@ -5,14 +5,14 @@ export class AddReadPermissionToPost20250428123000
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      ALTER TABLE "post"
+      ALTER TABLE "post_entity"
       ADD COLUMN "read_permission" "roles_enum" NULL
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      ALTER TABLE "post"
+      ALTER TABLE "post_entity"
       DROP COLUMN "read_permission"
     `);
   }
