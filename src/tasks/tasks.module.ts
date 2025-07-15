93101f4eb6766a0d1f02d8793726b6cd119f9cbe
Bumang-Cyber <calmness0729@gmail.com>
2025-07-16 00:23:56 +0900

    postsModule을 taskModule에 추가

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { PostsModule } from 'src/posts/posts.module';
 
 @Module({
+  imports: [PostsModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
